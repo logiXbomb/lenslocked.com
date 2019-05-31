6d7b63ba1386838bc533a328c22d64a2fe9be933
Git <slowie@powerchordsystem.com>
2019-05-31 18:26:58 -0400

    added simple router

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Waffles are great!!</h1>")
+
+	if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please get in touch <a href=\"mailto:support@test.com\">Support</a>.")
+	} else {
+		fmt.Fprint(w, "<h1>Waffles are great!!</h1>")
+	}
 }
 
 func main() {
