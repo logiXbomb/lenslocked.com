bc1047fe0930e5ebd116f717bf5de096098eb4f4
Git <slowie@powerchordsystem.com>
2019-06-01 12:07:34 -0400

    switched to gorilla mux

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Waffles are great!!</h1>")
+}
 
-	if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please get in touch <a href=\"mailto:support@test.com\">Support</a>.")
-	} else if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Waffles are great!!</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h3>404 ~ Page not found</h3>")
-	}
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please get in touch <a href=\"mailto:support@test.com\">Support</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := mux.NewRouter()
 
-	http.ListenAndServe(":3000", nil)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":3000", r)
 }
