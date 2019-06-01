0c874ca9d4f4e308bc1d3ec8be3c8a959c46d791
Git <slowie@powerchordsystem.com>
2019-06-01 12:13:43 -0400

    added faq route

--- main.go
+++ main.go
@@ -17,11 +17,17 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please get in touch <a href=\"mailto:support@test.com\">Support</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h2>What's going on?</h2>")
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 
 	http.ListenAndServe(":3000", r)
 }
