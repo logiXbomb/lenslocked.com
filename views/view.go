bee2e231a8ae76bb22b29286fdc3594231be36af
Seth Lowie <slowie@powerchordsystem.com>
2019-06-11 17:59:58 -0400

    finished 6.7

--- views/view.go
+++ views/view.go
@@ -1,13 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = "tmpl"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.tmpl",
-		"views/layouts/footer.tmpl",
-		"views/layouts/navbar.tmpl",
-	)
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -23,4 +28,15 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of strings
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+
+	if err != nil {
+		log.Fatalf("-- could not Glob layouts: %v", err)
+	}
+
+	return files
 }
